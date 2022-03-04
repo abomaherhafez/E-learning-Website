fbbdfdb93e89ff55f025a9b09722fe75c96338e0
abomaherhafez <mhmad.hafez@hotmail.com>
2022-03-04 14:38:44 +0100

    init

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import NavBar from "./components/NavBar";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import About from "./components/Pages/About";
+import { Blog } from "./components/Pages/Blog";
+import Home from "./components/Pages/Home";
+import Contact from "./components/Pages/Contact";
+import SignIn from "./components/Pages/SignIn";
+import HomeStudint from "./student/HomeStudint";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <NavBar />
+
+        <div className="pages">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/sign-in" element={<SignIn />} />
+            <Route path="/blog" element={<Blog />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/HomeStudent" element={<HomeStudint />} />
+            <Route path="/Create-account" element={<HomeStudint />} />
+          </Routes>
+        </div>
+      </Router>
+    </>
   );
 }
 
---
+++ client/src/components/cards/Card.js
@@ -0,0 +1,52 @@
+import "./Cards.css";
+import React from "react";
+import {
+  MDBCard,
+  MDBCardBody,
+  MDBCardTitle,
+  MDBCardText,
+  MDBCardImage,
+  MDBBtn,
+  MDBRipple,
+} from "mdb-react-ui-kit";
+import { Link } from "react-router-dom";
+
+export default function Card() {
+  return (
+    <div className="za3ma">
+      <div className="bb">
+        <MDBCard className="center" style={{ maxWidth: "28rem" }}>
+          <MDBRipple
+            rippleColor="light"
+            rippleTag="div"
+            className="bg-image hover-overlay"
+          >
+            <MDBCardImage
+              src="https://static.scientificamerican.com/blogs/cache/file/15E43A6F-3941-471A-BF4A81AECDC8C8AA_source.jpg?w=590&h=800&19535223-FF51-4AE3-A05064AFC6E375F5"
+              fluid
+              alt="..."
+            />
+            <a>
+              <div
+                className="mask"
+                style={{ backgroundColor: "rgba(251, 251, 251, 0.15)" }}
+              ></div>
+            </a>
+          </MDBRipple>
+          <MDBCardBody>
+            <MDBCardTitle>Card title</MDBCardTitle>
+            <MDBCardText className="class">
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </MDBCardText>
+            <MDBBtn>
+              <Link to="/HomeStudent">
+                <text className="pp">Button</text>
+              </Link>
+            </MDBBtn>
+          </MDBCardBody>
+        </MDBCard>
+      </div>
+    </div>
+  );
+}
