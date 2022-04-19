c41bbeaa7cfe6eb9e142c81e3c6fb308eeacba20
ihebguizani <iheb.guizani62@gmail.com>
2022-04-19 11:28:56 +0200

    first push

---
+++ server/config/mongoose.config.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+const express = require('express');
+
+
+mongoose.connect('mongodb+srv://panzouz:1234@cluster0.k5zjw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !')); 
+module.exports = conf;
---
+++ server/models/assisterSeance.js
@@ -0,0 +1,7 @@
+const mongoose = require("mongoose");
+const EtudiantSchema = new mongoose.Schema(
+    {
+        idE: {type: String},
+        idS: {type: String}
+    }
+);
