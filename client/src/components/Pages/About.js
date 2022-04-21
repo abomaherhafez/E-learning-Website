ee6c337bb81c42d1fc795d136cfe6f76ded68c89
NeirouzJbira <jbira.neirouz@gmail.com>
2022-04-21 10:33:13 +0100

    init

---
+++ client/src/components/Pages/About.js
@@ -0,0 +1,21 @@
+import React from "react";
+import Card from "../cards/Card";
+import Card1 from "../cards/Card1";
+import Footer from "../footer/Footer";
+import NavBar from "./NavBar";
+import "./About.css";
+export default function About() {
+  const x = window.location.href;
+  console.log(x);
+  return (
+    <div>
+      <NavBar />
+      <div className="about">
+        <Card1 />
+        <Card1 />
+        <Card1 />
+      </div>
+      <Footer />
+    </div>
+  );
+}
